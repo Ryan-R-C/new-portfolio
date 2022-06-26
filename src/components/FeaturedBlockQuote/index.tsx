350cafe76a731f7e065f8cc2b1ea31f5a7a8e212
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-06-26 02:02:14 -0300

    created the quotes component

---
+++ src/components/FeaturedBlockQuote/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {  Container } from './styles';
+
+import { BsQuestion } from 'react-icons/bs';
+import { ImQuotesLeft } from 'react-icons/im';
+
+
+export default function FeaturedBlockQuote(
+    { quote, author }: any
+    ) {
+  return (
+    <Container>
+        <div className='quotes-icon-container'>
+
+            <ImQuotesLeft/>
+
+        </div>
+        <blockquote>
+            {quote}
+        </blockquote>
+        <p className='author'>- {author}</p>
+    </Container>
+  );
+}
+
---
+++ src/components/FeaturedBlockQuote/styles.ts
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  position: relative;
+
+  flex-direction: column;
+  width: 75%;
+  background: #f5f5f5;
+
+  padding: 30px; 
+  margin: 15% auto;
+
+  blockquote{
+    margin-bottom: 2.75%;
+    font-size: 2rem;
+    color: #3a3a3a;
+  }
+
+  .author{
+    font-size: 1rem;
+    color: #abb8c3;
+    font-weight: bold;
+  }
+
+  .quotes-icon-container{
+    background:  #f5f5f5;
+    display: flex;
+    width: fit-content;
+    padding: 5px;
+    border: 1px solid black;
+    border-radius: 50%;
+    position: absolute;
+    left: 50%;
+    top: -6%;
+
+    svg{
+      font-size: 1.5rem;
+    }
+  }
+
+  
+
+`;
+
+
+
