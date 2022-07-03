b2c48ce8c645e189f86bffc27f5961ecc604637d
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-02 22:13:01 -0300

    refractored the component, removed unsefull imports

--- src/components/Landing/ImageContainer/index.tsx
+++ src/components/Landing/ImageContainer/index.tsx
@@ -1,28 +1,17 @@
-import React from 'react';
-import PortfolioParagraph from '../../Portfolio/PortfolioParagraph';
-import PortfolioImage from '../../Portfolio/PortfolioImage';
 import LandingImage from '../LandingImage';
 import { Aside } from './styles';
 
-import { BsQuestion } from 'react-icons/bs';
-import { IoBag } from 'react-icons/io5';
-import FeaturedBlockQuote from '../../FeaturedBlockQuote';
-import Portfolio from '../../../pages/Portfolio';
-import AboutMe from '../AboutMe';
-
-
 export default function ImageContainer(
-    // { title, link, src, paragraph }: any
-    ) {
+  { landingImageSrc }: any
+) {
   return (
-    < >
+    <>
       <Aside>
-          <LandingImage
-          imageLink=""
-          imageSrc="https://avatars.githubusercontent.com/u/88540088?v=4"
-          />
+        <LandingImage
+          imageSrc={landingImageSrc}
+        />
       </Aside>
-      </>
+    </>
   );
 }
 
