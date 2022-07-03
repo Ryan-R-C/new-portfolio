0e42b42bb819e59363c384f3fef86f30455b1a89
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-03 19:42:16 -0300

    removed warnings

--- src/components/ComputerContainer/index.tsx
+++ src/components/ComputerContainer/index.tsx
@@ -1,10 +1,4 @@
-import React from 'react';
-
 import {  Container } from './styles';
-
-import { BsQuestion } from 'react-icons/bs';
-import { ImQuotesLeft } from 'react-icons/im';
-
 
 export default function ComputerContainer(
     { imgSrc }: any
