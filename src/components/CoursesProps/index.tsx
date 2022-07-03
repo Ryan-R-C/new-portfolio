79025c6f2a326906a1e088ce9239e45be71c905a
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-02 21:55:54 -0300

    created the individual component to display a single course/skill item

---
+++ src/components/CoursesProps/index.tsx
@@ -0,0 +1,51 @@
+import { dir } from "console";
+import React, { useState } from "react";
+import ImageContainer from "../Landing/ImageContainer";
+import ImageContainerCourse from "../Landing/ImageContainerCourse";
+import { Container, ContainerText, ContaneirImage } from "./styles";
+
+
+export default function CoursesProps({ title, text, iconImageSrc, direction, mainColor, secondaryColor }: any) {
+  return (
+    <>
+      <Container
+        style={
+          {
+            flexDirection: direction,
+          }
+        }
+      >
+        <ContainerText>
+          <h3>{title}</h3>
+          {
+            text.map(
+              (textProp: any) => (
+                <p>
+                  {textProp}
+                </p>
+              )
+            )
+          }
+        </ContainerText>
+
+        <ContaneirImage
+          style={
+            {
+              backgroundImage: `linear-gradient(139deg, ${mainColor} 0%,${secondaryColor} 100%)`,
+            }
+          }
+
+          className={direction == 'row' ? "row" : 'reserse-row'}
+        >
+
+          <ImageContainerCourse
+            landingImageSrc={iconImageSrc}
+            direction={direction}
+          />
+
+        </ContaneirImage>
+      </Container>
+    </>
+  );
+}
+
---
+++ src/components/CoursesProps/styles.ts
@@ -0,0 +1,138 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+
+display: flex;
+width: 80%;
+margin: 0 auto;
+
+
+margin-top: 10%;
+/*
+padding: 10%;
+*/
+display: flex;
+justify-content: center;
+align-content: center;
+align-items: center;
+
+@media(max-width: 700px) {
+  flex-direction: column !important;
+}
+
+
+
+h2{
+  text-align: center;
+  margin-bottom: 100px;
+}  
+
+
+
+.row{
+  margin-left: 50px;
+}
+.reserse-row{
+  margin-right: 50px;
+}
+
+
+  .row, .reserse-row{
+    @media(max-width: 700px) {
+      margin: 0;
+      padding: 30px 0;
+    }
+  }
+
+
+`;
+
+
+export const ContainerText = styled.div`
+width: 70%;
+
+h3{
+  font-size: 32px;
+  text-align: center;
+  margin-bottom: 100px;
+}
+
+
+p{
+  margin-bottom: 25px;
+
+}
+
+
+div{
+  
+}
+
+.text-center{
+  text-align: center;
+}
+`;
+
+
+
+export const ContaneirImage = styled.div`
+
+
+display: flex;
+width: 100%;
+max-width: 350px;
+
+
+display: flex;
+justify-content: center;
+align-content: center;
+align-items: center;
+  
+position: relative;
+
+
+
+height: 30%;
+
+
+div{
+  
+  position: relative;
+  height: 100%;
+  
+  position: relative;
+  
+  height: 0;
+  
+ min-height: 300px;
+ min-width: 300px;
+
+ @media(max-width: 700px) {
+  min-height: 130px;
+  min-width: 150px;
+}
+
+
+}
+
+  .icon-image{
+    top:0;
+    bottom:0;
+    left:0;
+    right:0;
+    /*
+    position: absolute;
+    height: 100%;
+    */
+   width: 100%;
+
+  
+  }
+
+  
+  
+
+  
+`;
