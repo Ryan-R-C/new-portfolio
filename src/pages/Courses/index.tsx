d7e6e0d5f5ac4c52cf0e297486b7df35a54a397f
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-03 16:18:42 -0300

    created a courses component

---
+++ src/pages/Courses/index.tsx
@@ -0,0 +1,46 @@
+import { dir } from "console";
+import React, { useState } from "react";
+import CoursesProps from "../../components/CoursesProps";
+
+//@ts-ignore
+
+import { Container, ContainerText, ContaneirImage } from "./styles";
+
+
+export default function Courses({ props }: any) {
+
+
+  return (
+    <>
+      <Container>
+
+        <h2>
+          Skills
+        </h2>
+
+        {
+          props?.map(
+            (prop: any, index: number) => (
+              <CoursesProps
+                title={prop.title}
+                text={prop.text}
+                direction={index % 2 ? "row-reverse" : "row"}
+                iconImageSrc={prop.iconImageSrc}
+                mainColor={prop.mainColor}
+                secondaryColor={prop.secondaryColor}
+              >
+              </CoursesProps>
+
+            )
+          )
+        }
+
+
+
+      </Container>
+
+
+    </>
+  );
+}
+
---
+++ src/pages/Courses/styles.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+margin-top: 10%;
+
+h2{
+  text-align: center;
+  margin-bottom: 100px;
+}  
+
+
+
+
+`;
