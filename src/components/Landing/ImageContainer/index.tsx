bc72f31749ad229f73c8f65220dfae46e2504a2c
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-02 22:26:38 -0300

    now it passes the socialMedia links to LandingImage

--- src/components/Landing/ImageContainer/index.tsx
+++ src/components/Landing/ImageContainer/index.tsx
@@ -2,13 +2,17 @@ import LandingImage from '../LandingImage';
 import { Aside } from './styles';
 
 export default function ImageContainer(
-  { landingImageSrc }: any
+  { landingImageSrc, linkedingLink ,gitHubLink ,emailLink}: any
 ) {
   return (
     <>
       <Aside>
         <LandingImage
           imageSrc={landingImageSrc}
+          linkedingLink={linkedingLink}
+          gitHubLink={gitHubLink}
+          emailLink={emailLink}
+          
         />
       </Aside>
     </>
