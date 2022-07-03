1f7fd882c28c4ec364f3be4ce4963cea5c10456d
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-03 16:25:17 -0300

    refractored the PortfolioImage's css

--- src/components/Portfolio/PortfolioImage/styles.ts
+++ src/components/Portfolio/PortfolioImage/styles.ts
@@ -7,10 +7,24 @@ justify-content: center;
 box-shadow: rgba(0, 0, 0, 0.25) 0px 0.0625em 0.0625em,
 rgba(0, 0, 0, 0.25) 0px 0.125em 0.5em,
 rgba(255, 255, 255, 0.1) 0px 0px 0px 1px inset;
-
+height: 100%;
+width: 100%;
 margin: 0 auto;
 position: relative;
 z-index: 1;
+
+  h2{
+    
+    position: absolute;
+    left: 0;
+    right: 0;
+
+    bottom: 0;
+    margin: auto;
+    margin-bottom: 10px;
+    color: white;
+    z-index:1;
+  }
 
   &:before {
     transform: translateX(0%);
@@ -26,20 +40,55 @@ z-index: 1;
     background: #0000008a;
     right: 0;
     transform: translateX(1.05%) rotate(2deg) translateY(-2.5%);
+
+    transition: all 0.2s; 
+    }
+    
+    div{
+      overflow: hidden;
+      height: 100%;
+      width: 100%;
+      display: flex;
+
+      img {
+  
+          width: 100%;
+          height: 300px;
+  
+          background-color: blue;  
+          margin: 0 auto;
+          position: relative;
+          width: 100%;
+          object-fit: cover;
+          
+          transition: all 0.2s; 
+          transition-timing-function: cubic-bezier(0,0,.2,1);
+          transform: scale(1.03);
+      }
+    }
+    &:after{
+      position: absolute;
+      top: 0;
+      right: -1px;
+      bottom: -1px;
+      left: -1px;
+      background-image: linear-gradient(180deg,transparent 25%,#00000082 90%);
+      content: "";
+      transition-property: background-color,border-color,color,fill,stroke,opacity,box-shadow,transform;
+      transition-duration: .5s;
+      transition-timing-function: cubic-bezier(0,0,.2,1);
     }
 
-    img {
+    &:hover{
 
-        min-height: 190px;
-        width: 100%;
-        height: 10%
+      img{
+        transform: translateZ(0);
+      }
 
-        background-color: blue;  
-        margin: 0 auto;
-        position: relative;
-        width: 100%;
-        height: 10;
-        object-fit: cover;
-        }
+      &:before {
+
+        transform: translateX(0%) rotate(0) translateY(-2.5%);
+        background: #0000008a;
+      }
     }
 `;
