ce13d66d487c004a8da3c96f0844d7e46f9a6eba
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-03 12:53:50 -0300

    created a function to handle clicks outside the component, pre requisite to dynamic menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,40 +1,73 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 
-import {  Container, MainItem, SubItem, UnbreakerPage } from './styles';
+import { Container, MainItem, SubItem, UnbreakerPage } from './styles';
 
-import { BsQuestion } from 'react-icons/bs';
-import { ImQuotesLeft } from 'react-icons/im';
+const useOutsideClick = (callback: { (): void; (): void; }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    const handleClick = (event: { target: any; }) => {
+      //@ts-ignore
+      if (ref.current && !ref.current.contains(event.target)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('click', handleClick);
+
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [ref]);
+
+  return ref;
+};
+
 
 
 export default function FeaturedBlockQuote(
-    { quote, author }: any
-    ) {
+  { quote, author }: any
+) {
+  const [ isOpen, setIsOpen ]= useState(false)
+
+
+  const handleClickOutside = () => {
+    setIsOpen(false);
+  };
+
+  const ref = useOutsideClick(handleClickOutside);
+
   return (
     <>
-    <Container>
-      <MainItem>
-      </MainItem>
-      <SubItem>
-        <div className="container">
-          <a href="#">
-            Landing
-          </a>
-          <a href="#AboutMe">
-            About Me
-          </a>
-          <a href="#Portfolio">
-            Portfolio
-          </a>
-          <a href="#Timeline">
-            Timeline
-          </a>
-          <a href="#Courses">
-            Courses
-          </a>
-        </div>
-      </SubItem>
-    </Container>
-    <UnbreakerPage></UnbreakerPage>
+      {/* @ts-ignore */}
+      <Container ref={ref}  className="click-text">
+        <MainItem>
+        </MainItem>
+
+
+        <SubItem>
+          <div className="container">
+            <a href="#">
+              Landing
+            </a>
+            <a href="#AboutMe">
+              About Me
+            </a>
+            <a href="#Portfolio">
+              Portfolio
+            </a>
+            <a href="#Timeline">
+              Timeline
+            </a>
+            <a href="#Courses">
+              Courses
+            </a>
+          </div>
+        </SubItem>
+
+
+      </Container>
+      <UnbreakerPage></UnbreakerPage>
     </>
   );
 }
