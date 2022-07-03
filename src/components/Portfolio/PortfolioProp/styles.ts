7b21264af48d5cb636934295e7b8fe0920d66b55
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-03 16:25:42 -0300

    refractored the PortfolioProps' css

--- src/components/Portfolio/PortfolioProp/styles.ts
+++ src/components/Portfolio/PortfolioProp/styles.ts
@@ -7,6 +7,7 @@ export const Container = styled.div`
   flex-direction: column;
   background-color: blue;
   width: 100%;
+  height: 100%;
 `;
 
 
@@ -18,6 +19,7 @@ export const ProjectContainer = styled.div`
   flex-direction: column;
   background-color: white;
   width: 100%;
+  height: 100%;
 
   max-width: 100%
 `;
@@ -31,12 +33,16 @@ export const Project = styled.div`
   background-color: white;
   max-width: 100%;
   margin: 20px  5%;
+  height: 100%;
+  width: 100%;
   position: relative;
+
 
 
   
   h2 { 
     font-size: 40px;
     margin-bottom: 20px;
+    text-align: center;
   }
 `;
