dcb19f080f84efb521e634dc5ce9d248889ed74a
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-02 22:27:34 -0300

    now it displays the proper links in the component

--- src/components/Landing/LandingImage/index.tsx
+++ src/components/Landing/LandingImage/index.tsx
@@ -1,21 +1,17 @@
 
 import { Container, SocialMedia } from './styles';
-
 import { GrLinkedinOption } from 'react-icons/gr'
 import { BsGithub } from 'react-icons/bs'
 import { MdEmail } from 'react-icons/md'
 
-
-
-
-const LandingImage = ({  imageSrc }: { imageSrc:string }) => {
+const LandingImage = ({  imageSrc, linkedingLink ,gitHubLink ,emailLink }: { imageSrc:string,  linkedingLink:string ,gitHubLink:string ,emailLink:string}) => {
   return (
     <>
     <Container>
       <SocialMedia>
-        <a className="button" href=''> <GrLinkedinOption/> </a>
-        <a className="button" href=''> <BsGithub/> </a>
-        <a className="button" href='mailto:someone@yoursite.com'> <MdEmail/> </a>
+        <a className="button" href={linkedingLink}> <GrLinkedinOption/> </a>
+        <a className="button" href={gitHubLink}> <BsGithub/> </a>
+        <a className="button" href={`mailto:${emailLink}`}> <MdEmail/> </a>
       </SocialMedia>
           <img
               src={imageSrc}
