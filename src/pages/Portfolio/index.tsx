7351f63a5bac804931c7b1e4fe71522f36922e16
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-03 16:18:53 -0300

    created a portfolio component

---
+++ src/pages/Portfolio/index.tsx
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from 'react';
+import PortfolioParagraph from '../../components/Portfolio/PortfolioParagraph';
+import PortfolioImage from '../../components/Portfolio/PortfolioImage';
+import PortfolioProp from '../../components/Portfolio/PortfolioProp';
+import { Container,  FilterSelector, GridContainer, GridContainerPortfolioItems, GridItem, GridItem2, ModalContainer, ModalContent, } from './styles';
+import IportfolioInfo from '../../types/IportfolioInfo'
+import PortfolioProp2 from '../../components/Portfolio2/PortfolioProp2';
+
+import { FiX } from 'react-icons/fi'
+//@ts-ignore
+import Modal from 'react-modal';
+import ModalPortfolioProp from '../../components/Portfolio/ModalPortfolioProp';
+// import ModalPortfolioProp from '../../components/Portfolio/ModalPortfolioProp';
+
+let modalStyles={
+  overlay: {
+    position: 'fixed',
+    zIndex: 1020,
+    top: 0,
+    left: 0,
+    right: 0,
+    inset: 0,
+    width: '100vw',
+    height: '100vh',
+    background: 'rgba(255, 255, 255, 0.75)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: "all .5s",
+  },
+  content: {
+    background: 'white',
+    width: '85%',
+    inset: 0,
+    maxWidth: 'calc(100vw - 2rem)',
+    maxHeight: 'calc(100vh - 2rem)',
+    overflowY: 'auto',
+    position: 'relative',
+    border: '1px solid #ccc',
+    borderRadius: '0.3rem',
+    height: '88%',
+    alignSelf: 'center',
+    transition: "all .5s"
+
+  }
+}
+
+export default function Portfolio({info, filters}: any) {
+/*======================================================================================================
+
+=========================================================================================================*/
+  const [displayedItems, setDisplayedItems] = useState<any>([]) 
+  const [filterSelected, setFilterSelected] = useState<any>() 
+
+  const [selectedItem, setSelectedItem] = useState<any>({}) 
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModalAndSetValues(selectedPortolioItem: any) {
+    setSelectedItem(selectedPortolioItem.detailedInfo)
+    console.log(selectedPortolioItem.detailedInfo)
+    
+    openModal()
+  }
+
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  useEffect(
+    () => {
+      setDisplayedItems(info)
+    }, [info]
+  )
+
+
+
+  function filterPorfolio (filterSelected: string) {
+    let filteredComponent: any = []
+
+    info.map(
+      (portfolioItem: any) => {
+        console.log(portfolioItem.filter == filterSelected)
+        if(portfolioItem.filter == filterSelected) filteredComponent.push(portfolioItem)
+      }
+    )
+
+
+    setDisplayedItems(filteredComponent)
+    setFilterSelected(filterSelected)
+
+  }
+
+
+  function clearFilterPorfolio () {
+    setDisplayedItems(info)
+    setFilterSelected("")
+  }
+
+  return (
+    <>
+    <Container>
+      <FilterSelector>
+        <button 
+        className={!filterSelected ? "activeFilter" : ""}
+        onClick={() => clearFilterPorfolio()}>
+          Tudo
+        </button>
+        {
+        filters?.map(
+          (filter: string) => (
+            <button
+            key={filter}
+            className={filter == filterSelected ? "activeFilter" : ""}
+            onClick={() => filterPorfolio(filter)}>
+              {filter}
+            </button>
+
+          )
+        )
+        }
+      </FilterSelector>
+      {/* <GridContainer>
+        {
+
+        displayedItems?.map(
+          (e: any, i: number) => (
+            <GridItem key={i}>
+        
+              <PortfolioProp
+              title={e?.title}
+              link={e?.link}
+              src={e?.src}
+              paragraph={e?.paragraph}
+              />
+            </GridItem>
+            
+          )
+        )
+
+        }
+      </GridContainer> */}
+    </Container>
+
+    <>
+      <GridContainerPortfolioItems>
+        {
+
+        displayedItems?.map(
+          (e: any, i: number) => (
+            <GridItem2 key={i}>
+        
+              <PortfolioProp2
+              title={e?.title}
+              link={e?.link}
+              src={e?.src}
+              paragraph={e?.paragraph}
+              onClickEvent={() => openModalAndSetValues(e)}
+              />
+            </GridItem2>
+            
+          )
+        )
+
+        }
+      </GridContainerPortfolioItems>
+    </>
+    <>
+      <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          //@ts-ignore
+          style={
+            modalStyles
+          }
+          >
+          <ModalContainer>
+            <FiX
+            onClick={closeModal}
+            className="closeModal"
+            size={23}
+            />
+            <ModalContent>
+              <ModalPortfolioProp
+              infos={    selectedItem?.infos}
+              link={     selectedItem?.link}
+              linkTitle={selectedItem?.linkTitle}
+              techs={    selectedItem?.techs}
+              imgSrc={   selectedItem?.imgSrc}
+              title={     selectedItem?.title}
+
+              />
+ 
+            </ModalContent>
+
+        </ModalContainer>
+        </Modal>
+    </>
+
+
+    </>
+  );
+}
+
---
+++ src/pages/Portfolio/styles.ts
@@ -0,0 +1,136 @@
+//@ts-ignore
+import { Modal } from 'react-modal';
+import styled from "styled-components";
+
+
+
+export const Container = styled.div`
+display: flex;
+
+flex-wrap: wrap;
+
+flex-direction: column;
+padding: 0 10%;
+/*
+align-items: center;
+justify-content: center;
+background-color: blue;
+
+width: 100%;
+*/
+max-width: 100%;
+padding: 30px;
+`;
+
+
+export const GridContainer = styled.div`
+display: flex;
+
+flex-wrap: wrap;
+padding: 30px;
+margin: 0 10px;
+
+
+/*
+align-items: center;
+flex-direction: column;
+justify-content: center;
+background-color: blue;
+width: 100%;
+
+*/
+`;
+
+export const GridItem = styled.div`
+height: 100%;
+/*
+width: 100%;
+*/
+height: 501px;
+
+
+
+@media(min-width: 900px) {
+  width: calc(50% - 40px);
+  padding: 0 20px;
+}
+
+`;
+
+
+
+export const GridItem2 = styled.div`
+height: 100%;
+width: 100%;
+/*
+height: 500px;
+*/
+height: 60vh;
+
+box-shadow: rgba(0,0,0,0.4) 0px 2px 4px,rgba(0,0,0,0.3) 0px 7px 13px -3px,rgba(0,0,0,0.2) 0px -3px 0px inset;
+margin: 1.25% 0;
+
+@media(min-width: 900px) {
+  
+  width: 48.8%;
+  margin-bottom: 1.25%;
+}
+
+`;
+
+
+export const FilterSelector = styled.div`
+display:flex;
+
+button{
+
+  color: #666;
+  padding: 10px 20px;
+  border: 1px solid #e2e2e2;
+  display: block;
+  height: auto;
+  overflow: hidden;
+  margin: -1px -1px 0 0;
+  transition: all 0.2s;
+  
+}
+
+.activeFilter{
+  color: #141617 !important;
+}
+`;
+
+
+
+
+
+export const GridContainerPortfolioItems = styled.div`
+width: 95vw;
+display: flex;
+flex-wrap: wrap;
+margin: 0 auto;
+padding: 2.5%;
+justify-content: space-between;
+
+`
+
+export const ModalContainer = styled.div`
+display: flex;
+flex-direction: column;
+
+.closeModal{
+  align-self: flex-end;
+  position: fixed;
+}
+
+
+
+
+
+
+`
+
+export const ModalContent = styled.div`
+padding: 5%;
+
+`
