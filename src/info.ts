0dcdcc395670090b0192c97d932d7d67a9024be3
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2022-07-17 15:34:45 -0300

    created the headerLinks in info

--- src/info.ts
+++ src/info.ts
@@ -48,6 +48,15 @@
 
 export const storagedInfo =
 {
+
+  headerLinks: {
+    landing: "Início", 
+    aboutMe: "Sobre",
+    portfolio: "Portfolio",
+    timeLine: "Timeline",
+    courses: "Skills"
+  },
+
   aboutMeQuote: { // V
     quote: "Uma máquina consegue fazer o trabalho de 50 homens ordinários. Nenhuma máquina consegue fazer o trabalho de um homem extraordinário.",
     author: "Elbert Hubbard, escritor."
