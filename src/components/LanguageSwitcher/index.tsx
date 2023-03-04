27c589befaa1be57437bb5c2ff84fcd07339d604
Ryan-R-C <ryan.r.c.339ac@gmail.com>
2023-03-04 17:49:41 -0300

    feat: created the languageSwitcher component

---
+++ src/components/LanguageSwitcher/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactFlagsSelect from "react-flags-select";
+import { Container } from "./styles";
+
+const LanguageSwitcher = ({selected, setSelected}: any) => {
+
+  return (
+   <Container>
+    <ReactFlagsSelect
+      // onSelect={(code) => console.log(code)}
+      onSelect={(code) => setSelected(code)}
+      selected={selected}
+      countries={["US", "BR"]}
+      customLabels={{ US: "EN-US", BR: "PT-BR"}}
+      placeholder="Select Language"
+      searchPlaceholder="Search countries"
+    />
+   </Container>
+  );
+};
+
+export default LanguageSwitcher;
---
+++ src/components/LanguageSwitcher/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+.ReactFlagsSelect-module_flagsSelect__2pfa2{
+  padding: 0;
+}
+`;
